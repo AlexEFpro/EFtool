338572b5e082ca663288edf6be4299d834aa8a90
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2024-03-06 09:18:01 -0600

    Initial Commit

---
+++ EFtool/EFtoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  EFtoolApp.swift
+//  EFtool
+//
+//  Created by Alejandro  Salinas Ramirez on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct EFtoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
